03313314b48a78c83aaf0c095c982bb0c1b1b5db
Zlobinnn <zlobin.savva@mail.ru>
2021-11-13 23:07:56 +0300

    Added definitions for an error when entering a sentence

--- ZlobinSV/lab_1/main.cpp
+++ ZlobinSV/lab_1/main.cpp
@@ -5,16 +5,16 @@ using namespace std;
 int main()
 {
     int i = 0, zam2, n = 0, k = 0, sum[100];
-    string zam1,str,word[100];
+    string zam1, str, word[100];
     char a;
-    
+
     cout << "Enter the string: ";
 
     getline(cin, str);
-    
+
     str += " ";
 
-     while (i < str.length()-1) {
+    while (i < str.length() - 1) {
         while (str[i] != ' ' && str[i] != ',' && str[i] != '.' && str[i] != '!' && str[i] != '?') {
             word[n] += str[i];
             i++;
@@ -24,10 +24,25 @@ int main()
             word[n] += str[i];
             i++;
         }
+        if (word[n].length()>=2) {
+            if ((word[n][0] == ',' || word[n][0] == '.' || word[n][0] == '!' || word[n][0] == '?') && (word[n][1] == ',' || word[n][1] == '.' || word[n][1] == '!' || word[n][1] == '?')) {
+                cout << "Error! The pinching mark is in the middle of a word or more than one pinning mark appears after / before the word.";
+                return 0;
+            }
+            if ((word[n][word[n].length() - 1] == ',' || word[n][word[n].length() - 1] == '.' || word[n][word[n].length() - 1] == '!' || word[n][word[n].length() - 1] == '?') && (word[n][word[n].length() - 2] == ',' || word[n][word[n].length() - 2] == '.' || word[n][word[n].length() - 2] == '!' || word[n][word[n].length() - 2] == '?')) {
+                cout << "Error! The pinching mark is in the middle of a word or more than one pinning mark appears after / before the word.";
+                return 0;
+            }
+        }
+        if (word[n] == "," || word[n] == "." || word[n] == "!" || word[n] == "?") {
+            cout << "Error! The pinching mark is in the middle of a word or more than one pinning mark appears after / before the word.";
+            return 0;
+        }
+
         n++;
     }
-     n--;
-     
+    n--;
+
 
     for (int i = 0; i <= n; i++) {
         sum[i] = -1;
@@ -45,12 +60,12 @@ int main()
         if (sum[i] != -1) {
             for (int k = i + 1; k <= n; k++) {
                 if (sum[k] < sum[i] && sum[k] != -1 && sum[i] != -1) {
-                        zam1 = word[i];
-                        word[i] = word[k];
-                        word[k] = zam1;
-                        zam2 = sum[i];
-                        sum[i] = sum[k];
-                        sum[k] = zam2;
+                    zam1 = word[i];
+                    word[i] = word[k];
+                    word[k] = zam1;
+                    zam2 = sum[i];
+                    sum[i] = sum[k];
+                    sum[k] = zam2;
                 }
             }
         }
@@ -59,5 +74,5 @@ int main()
     for (int i = 0; i <= n; i++) {
         cout << word[i];
     }
-    
+
 }
