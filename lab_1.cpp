8d88dcfc97dcba9085d3f1dd769eaf68713f87a4
Dopedupe <94796254+Dopedupe@users.noreply.github.com>
2021-11-26 01:07:14 +0300

    Update and rename Untitled-1.cpp to lab_1.cpp

--- lab_1.cpp
+++ lab_1.cpp
@@ -41,6 +41,56 @@ int main() {
 		}
 
 	}
-	
 
+
+	for (int i = 0; i < acs.size(); i++) {
+		for (int j = i; j < acs.size(); j++) {
+			if (acs[i] > acs[j]) {
+				int buf;
+				buf = acs[i];
+				acs[i] = acs[j];
+				acs[j] = buf;
+				std::string buf_s;
+				buf_s = slova[i];
+				slova[i] = slova[j];
+				slova[j] = buf_s;
+			}
+
+		}
+
+	}
+	std::string res = "";
+	int res_c;
+	res_c = 0;
+	int k;
+	k = 1;
+	std::string hh;
+
+	hh = "";
+
+	for (int i : s) {
+		if ((i == (int)' ') || (i == (int)'.') || (i == (int)',') || (i == (int)'!') || (i == (int)'?')) {
+
+			if (k == 1) {
+				hh = slova[res_c + 2];
+				res += hh;
+				hh = " ";
+				k--;
+
+			}
+			if (k == 0) {
+				res += (char)i;
+				k++;
+			}
+
+			res_c++;
+
+
+		}
+
+
+	}
+
+	res = res.substr(0, res.size() - 1);
+	std::cout << res << std::endl;
 }
