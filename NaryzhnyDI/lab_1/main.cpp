bbfaa9a25c35ec792e50d6250199d685b6845549
NeyDen-1 <94805699+NeyDen-1@users.noreply.github.com>
2021-12-02 17:53:59 +0300

    Update main.cpp

--- NaryzhnyDI/lab_1/main.cpp
+++ NaryzhnyDI/lab_1/main.cpp
@@ -17,32 +17,31 @@ bool checking(char symbol){
 
 int main() {
     vector<string> words;
-    vector<int> znach;
-    vector<string> ok;
-    string str, the_word = "";
-    int a, b;
+    vector<int> ascii_znach;
+    vector<string> p_symbols;
+    string str, the_word;
+    int a;
     a = 0;
     cout << "Please, write any sentence below you'd like to: " << endl;
     getline(cin, str);
-    b = str.length();
-    while(a<b){
-        while ((checking(str[a]) == 1) && (a < b)) {
-            int so = 0;
+    while(a<str.length()){
+        while ((checking(str[a]) == 1) && (a < str.length())) {
+            int ascii_zn = 0;
             do {
                 the_word += str[a];
-                so += str[a];
+                ascii_zn += str[a];
                 a++;
-            } while ((checking(str[a]) == 1) && (a < b));
+            } while ((checking(str[a]) == 1) && (a < str.length()));
             words.push_back(the_word); // Here I put the user's word into the vector named "words"
             the_word.clear();
-            znach.push_back(so);
+            ascii_znach.push_back(ascii_zn);
         }
-        while ((checking(str[a]) != 1) && (a < b)) {
+        while ((checking(str[a]) != 1) && (a < str.length())) {
             do {
                 the_word += str[a];
                 a++;
-            } while ((checking(str[a]) == 0) && (a < b));
-            ok.push_back(the_word); //Here I put the user's punctuation symbol into the vector named "ok"
+            } while ((checking(str[a]) == 0) && (a < str.length()));
+            p_symbols.push_back(the_word); //Here I put the user's punctuation symbol into the vector named "p_symbols"
             the_word.clear();
 
         }
@@ -51,28 +50,28 @@ int main() {
 // I've used the Bubble sort
     for (int i = 0; i < words.size(); i++) {
         for (int j = 0; j < words.size() - 1; j++) {
-            if (znach[j] > znach[j + 1]) {
-                int b = znach[j];
-                znach[j] = znach[j + 1];
-                znach[j + 1] = b;
-                string a = words[j];
+            if (ascii_znach[j] > ascii_znach[j + 1]) {
+                int local2 = ascii_znach[j];
+                ascii_znach[j] = ascii_znach[j + 1];
+                ascii_znach[j + 1] = local2;
+                string local1 = words[j];
                 words[j] = words[j + 1];
-                words[j + 1] = a;
+                words[j + 1] = local1;
             }
         }
     }
-        if(words.size()==ok.size()){
+        if(words.size()==p_symbols.size()){
             for (int i = 0; i < words.size(); i++){
-                cout<<words[i]<<ok[i];
+                cout<<words[i]<<p_symbols[i];
             }
         }
         else{
-            int a;
-            for (int i = 0; i < ok.size(); i++){
-                cout<<words[i]<<ok[i+1];
-                a=i;
+            int local3;
+            for (int i = 0; i < p_symbols.size(); i++){
+                cout<<words[i]<<p_symbols[i+1];
+                local3=i;
             }
-            cout << words[a+1];
+            cout << words[local3+1];
         }
 
 }
