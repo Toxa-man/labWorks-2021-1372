e2730d3ca88d4bb1ad0ebfde473381941000ab27
KhabarovaEA <94763859+KhabarovaEA@users.noreply.github.com>
2021-12-10 16:05:21 +0300

    Update main.cpp

--- KhabarovaEA/main.cpp
+++ KhabarovaEA/main.cpp
@@ -3,13 +3,24 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include "Fuctions.h"
+
 using namespace std;
+bool isPunct(char& c)
+{
+	return c == '.' || c == ',' || c == '!' || c == '?' || c == ' ';
+}
 int sum(string word)
 {
 	int s = 0;
+	bool flag = false;
 	for (int i = 0; i < word.length(); i++)
-		if (word[i] != '.' && word[i] != ',' && word[i] != '!' && word[i] != '?')
+		if (!isPunct(word[i]))
 			s += word[i];
+		else
+			flag = true;
+	if (flag)
+		s *= -1;
 	return s;
 }
 
@@ -18,6 +29,22 @@ bool f(string left, string right)
 	return sum(left) < sum(right);
 }
 
+
+void cut(string& str, string& before, string& after)
+{
+	int b = 0, e = str.length() - 1;
+	if (isPunct(str[b]))
+	{
+		before = str[b];
+		b++;
+	}
+	if (isPunct(str[e]))
+	{
+		after = str[e];
+		e--;
+	}
+	str = str.substr(b, e - b + 1);
+}
 
 int main()
 {
@@ -28,22 +55,41 @@ int main()
 	vector<string> v, all;
 	while (sin >> str)
 	{
-		if (sum(str))
+		int s = sum(str);
+		string before = "";
+		string after = "";
+		if (s > 0)
 			v.push_back(str);
+		else if (s < 0)
+		{
+			cut(str, before, after);
+			v.push_back(str);
+		}
+		if (before != "")
+		{
+			all.push_back(before);
+			before = "";
+		}
 		all.push_back(str);
-
+		if (after != "")
+		{
+			all.push_back(after);
+			after = "";
+		}
+		all.push_back(string(" "));
 	}
 	sort(v.begin(), v.end(), f);
 	int j = 0;
 	for (int i = 0; i < all.size(); i++)
 	{
-		if (sum(all[i]))
+		int s = sum(all[i]);
+		if (s)
 		{
-			cout << v[j] << " ";
+			cout << v[j];
 			j++;
 		}
 		else
-			cout << all[i] << " ";
+			cout << all[i];
 	}
 	cout << endl;
 	return 0;
