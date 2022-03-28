58be70a698af6d35ac8a0b316adc6b57216c8b08
GavrilovMS <94075839+GavrilovMS@users.noreply.github.com>
2022-03-29 00:27:17 +0300

    Provided for possible errors.

--- GavrilovMS/lab_3/main.cpp
+++ GavrilovMS/lab_3/main.cpp
@@ -6,30 +6,33 @@ private:
 	size_t size, max_size;
 	Type* arr;
 public:
-
 	vector(size_t start_size = 0) {
 		size = start_size;
 		if (start_size == 0) {
-			max_size = 1;
+			max_size = 5;
 		}
 		else {
 			max_size = start_size * capacity;
-		}	
+			
+		}
+		
 		arr = new Type[max_size];
 	}
-  
 	~vector() {
 		delete arr;
 	}
-  
 	Type& operator[](size_t index) {
-		return arr[index];
+		if (index < size) {
+			return arr[index];
+		}
+		else {
+			std::cout << "Index more than size of vector";
+			exit;
+		}
 	}
-  
 	size_t getsize() {
 		return size;
 	}
-  
 	void push_back(const Type& element) {
 		if (size != max_size) {
 			arr[size++] = element;
@@ -49,37 +52,49 @@ public:
 	}
 
 	void push_in(size_t index, const Type& element) {
-		if (size != max_size) {
-			for (size_t i = size; i > index; i--) {
-				arr[i] = arr[i - 1];
+		if (index <= size) {
+			if (size != max_size) {
+				for (size_t i = size; i > index; i--) {
+					arr[i] = arr[i - 1];
+				}
+				arr[index] = element;
+				size++;
+				return;
 			}
-			arr[index] = element;
-			size++;
-			return;
+			else {
+				Type* curarr = arr;
+				max_size = size * capacity;
+				arr = new Type[max_size];
+				for (size_t i = 0; i < index; i++) {
+					arr[i] = curarr[i];
+				}
+				for (size_t i = size; i > index; i--) {
+					arr[i] = curarr[i - 1];
+				}
+				delete curarr;
+				arr[index] = element;
+				size++;
+				return;
+			}
 		}
 		else {
-			Type* curarr = arr;
-			max_size = size * capacity;
-			arr = new Type[max_size];
-			for (size_t i = 0; i < index; i++) {
-				arr[i] = curarr[i];
-			}
-			for (size_t i = size; i > index; i--) {
-				arr[i] = curarr[i - 1];
-			}
-			delete curarr;
-			arr[index] = element;
-			size++;
-			return;
+			std::cout << "Index more than size of vector";
+			exit;
 		}
 	}
 
 	void erase(size_t index) {
-		for (index ; index < size - 1; index++) {
-			arr[index] = arr[index + 1];
+		if (index < size) {
+			for (index; index < size - 1; index++) {
+				arr[index] = arr[index + 1];
+			}
+			size--;
+			return;
 		}
-		size--;
-		return;
+		else {
+			std::cout << "Index more than size of vector";
+			exit;
+		}
 	}
 	
 
@@ -97,81 +112,133 @@ private:
 	Node* head, * last_node;
 
 public:
-	list(size_t start_size = 1) {
-		head = new Node;
-		last_node = head;
-		size = start_size;
-		Node* cur = head;
-		Node* next;
-		for (size_t i = 1; i < start_size; i++) {
-			next = new Node;
-			cur->nextadr = next;
-			cur = next;
-			last_node = cur;
+	list (size_t start_size = 0) {
+		if (start_size != 0) {
+			head = new Node;
+			last_node = head;
+			size = start_size;
+			Node* cur = head;
+			Node* next;
+			for (size_t i = 1; i < start_size; i++) {
+				next = new Node;
+				cur->nextadr = next;
+				cur = next;
+				last_node = cur;
+			}
+		}
+		else {
+			head = nullptr;
+			last_node = nullptr;
 		}
 	}
-  
 	~list() {
-		Node* next ;
-		do {
+		Node* next;
+		while (head != nullptr) {
 			next = head->nextadr;
 			delete head;
 			head = next;
-		} while (head != nullptr);
-	}
-  
-	Type& operator[](size_t index) {
-		Node* cur = head;
-		for(size_t i = 0; (i < index)&&(index < size); i++) {
-			cur = cur->nextadr;
 		}
-		return cur->data;
+	}
+	Type& operator[](size_t index) {
+		if (index < size) {
+			Node* cur = head;
+			for (size_t i = 0; (i < index); i++) {
+				cur = cur->nextadr;
+			}
+			return cur->data;
+		}
+		else {
+			std::cout << "Index more than size of list" << std::endl;
+			exit;
+		}
 	}
 
 	void push_back(const Type& element) {
-		Node* new_node = new Node;
-		new_node->data = element;
-		last_node->nextadr = new_node;
-		last_node = new_node;
+		if (size != 0) {
+			Node* new_node = new Node;
+			new_node->data = element;
+			last_node->nextadr = new_node;
+			last_node = new_node;
+		}
+		else {
+			head = new Node;
+			head->data = element;
+			last_node = head;
+		}
 		size++;
+		
 	}
 
 	void push_in(size_t index, const Type& element) {
-		Node* new_node = new Node;
-		new_node->data = element;
-		Node* cur = head;
-		for (size_t i = 0; i < index-1; i++) {
-			cur = cur->nextadr;
+		if (index < size) {
+			if (index != 0) {
+				Node* new_node = new Node;
+				new_node->data = element;
+				Node* cur = head;
+				for (size_t i = 0; i < index - 1; i++) {
+					cur = cur->nextadr;
+				}
+				new_node->nextadr = cur->nextadr;
+				cur->nextadr = new_node;
+
+			}
+			else {
+				Node* new_node = new Node;
+				new_node->data = element;
+				new_node->nextadr = head;
+				head = new_node;
+				
+			}
+
+			size++;
 		}
-		new_node->nextadr = cur->nextadr;
-		cur->nextadr = new_node;
-		size++;
+		else {
+			if (size != 0) {
+				std::cout << "Index more than size of list" << std::endl;
+				exit;
+			}
+			else {
+				head = new Node;
+				head->data = element;
+				last_node = head;
+				size++;
+			}
+		}
 	}
 
 	void push_front(const Type& element) {
+		
 		Node* new_node = new Node;
 		new_node->data = element;
 		new_node->nextadr = head;
 		head = new_node;
+		if (size == 0) {
+			last_node = head;
+		}
 		size++;
 	}
 
 	void erase(size_t index) {
-		Node* cur = head;
-		Node* past_node = cur;
-		for (size_t i = 0;  i < index; i++) {
-			past_node = cur;
-			cur = cur->nextadr;
-		}
-		if (index == 0) {
-			head = cur->nextadr;
-			delete cur;
+		if (index < size) {
+			Node* cur = head;
+			if (index == 0) {
+				head = head->nextadr;
+				delete cur;
+			}
+			else {
+				for (size_t i = 0; i < index - 1; i++) {
+					cur = cur->nextadr;
+				}
+				Node* node_to_delete = cur->nextadr;
+				cur->nextadr = cur->nextadr->nextadr;
+				delete node_to_delete;
+			}
+			size--;
 		}
 		else {
-			past_node->nextadr = cur->nextadr;
-			delete cur;
+			std::cout << "Index more than size of list or list is empty" << std::endl;
+			exit;
 		}
-		size--;
 	}
 
 	size_t getsize() {
