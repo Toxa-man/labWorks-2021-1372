439890b3c1e665038da2b88dbf98a46358dbf0da
GavrilovMS <94075839+GavrilovMS@users.noreply.github.com>
2022-05-25 18:50:13 +0300

    Create main.cpp

---
+++ GavrilovMS/Messenger/main.cpp
@@ -0,0 +1,13 @@
+#include "messenger.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    
+    QApplication a(argc, argv);
+
+    Messenger w;
+    w.show();
+
+    return a.exec();
+}
